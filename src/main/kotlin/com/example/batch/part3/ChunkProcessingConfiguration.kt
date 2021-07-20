4cf27cbd8902ae8bfe65eaa48b591e0e65d89c15
dgahn <dgahn@si-analytics.ai>
2021-07-20 23:39:22 +0900

    tasklet vs chunk

---
+++ src/main/kotlin/com/example/batch/part3/ChunkProcessingConfiguration.kt
@@ -0,0 +1,87 @@
+package com.example.batch.part3
+
+import mu.KotlinLogging
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.support.ListItemReader
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+private val logger = KotlinLogging.logger { }
+
+@Configuration
+class ChunkProcessingConfiguration(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory
+) {
+
+    @Bean
+    fun chunkProcessingJob() = jobBuilderFactory.get("chunkProcessingJob")
+        .incrementer(RunIdIncrementer())
+        .start(this.taskBaseStep())
+        .next(this.chunkBaseStep())
+        .build()
+
+    @Bean
+    fun taskBaseStep() = stepBuilderFactory.get("taskBaseStep")
+        .tasklet(tasklet())
+        .build()
+
+    private fun tasklet(): Tasklet {
+        val items = getItems()
+
+        return Tasklet { contribution, chunkContext ->
+            val stepExecution = contribution.stepExecution
+
+            val chunkSize = 10
+            val fromIndex = stepExecution.readCount
+            val toIndex = fromIndex + chunkSize
+
+            if(fromIndex >= items.size) {
+                RepeatStatus.FINISHED
+            } else {
+                val subList = items.subList(fromIndex, toIndex)
+
+                logger.info { "task item size: ${subList.size}" }
+
+                stepExecution.readCount = toIndex
+
+                RepeatStatus.CONTINUABLE
+            }
+        }
+    }
+
+    private fun getItems(): List<String> {
+        val items = mutableListOf<String>()
+        for (i in 1..100) {
+            items.add("$i + hello")
+        }
+        return items
+    }
+
+    @Bean
+    fun chunkBaseStep() = stepBuilderFactory.get("chunkBaseStep")
+        .chunk<String, String>(10)
+        .reader(itemReader())
+        .processor(itemProcessor())
+        .writer(itemWriter())
+        .build()
+
+    private fun itemWriter(): ItemWriter<String> =
+        ItemWriter { items: List<String> -> logger.info { "chunk item size: ${items.size}" } }
+//        ItemWriter { items: List<String> -> items.forEach(logger::info) }
+
+
+    private fun itemProcessor(): ItemProcessor<String, String> {
+        return ItemProcessor { item: String -> "$item, Spring Batch" }
+    }
+
+    private fun itemReader(): ItemReader<String?> = ListItemReader(getItems())
+
+}
