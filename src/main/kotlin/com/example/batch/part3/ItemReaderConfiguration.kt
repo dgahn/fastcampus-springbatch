04c51ac4ea370e8e2fffa6738b03b7a10a4cadfa
dgahn <dgahn@si-analytics.ai>
2021-07-21 01:16:52 +0900

    add csv file item reader example

--- src/main/kotlin/com/example/batch/part3/ItemReaderConfiguration.kt
+++ src/main/kotlin/com/example/batch/part3/ItemReaderConfiguration.kt
@@ -7,8 +7,13 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
+import org.springframework.batch.item.file.mapping.DefaultLineMapper
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 
 private val logger = KotlinLogging.logger { }
 
@@ -22,6 +27,7 @@ class ItemReaderConfiguration(
     fun itemReaderJob(): Job = jobBuilderFactory.get("itemReaderJob")
         .incrementer(RunIdIncrementer())
         .start(customItemReaderStep())
+        .next(csvFileStep())
         .build()
 
     @Bean
@@ -30,6 +36,40 @@ class ItemReaderConfiguration(
         .reader(CustomItemReader(getItems()))
         .writer(itemWriter())
         .build()
+
+    @Bean
+    fun csvFileStep() = stepBuilderFactory.get("csvFilStep")
+        .chunk<Person, Person>(10)
+        .reader(csvFileItemReader())
+        .writer(itemWriter())
+        .build()
+
+    private fun csvFileItemReader(): FlatFileItemReader<Person> {
+        val lineMapper = DefaultLineMapper<Person>()
+        val tokenizer = DelimitedLineTokenizer()
+        tokenizer.setNames("id", "name", "age", "address")
+        lineMapper.setLineTokenizer(tokenizer)
+
+        lineMapper.setFieldSetMapper {
+            val id = it.readInt("id")
+            val name = it.readString("name")
+            val age = it.readString("age")
+            val address = it.readString("address")
+
+            Person(id, name, age, address)
+        }
+
+        return FlatFileItemReaderBuilder<Person>()
+            .name("csvFileItemReader")
+            .encoding("UTF-8")
+            .resource(ClassPathResource("test.csv"))
+            .linesToSkip(1)
+            .lineMapper(lineMapper)
+            .build()
+            .apply {
+                this.afterPropertiesSet() // 필수 프로퍼티에 대해 검증하는 메소드
+            }
+    }
 
     private fun itemWriter(): ItemWriter<in Person> = ItemWriter { items ->
         logger.info { items.joinToString { it.name } }
